c2f7b5c79417f81ecf87f5cc30743c002a103dea
Netcarver <id.github.com@gzap.org>
2020-09-24 22:22:54 +0100

    Implement some of PSR12

--- lib/Resque/Job/PID.php
+++ lib/Resque/Job/PID.php
@@ -8,36 +8,35 @@
  */
 class Resque_Job_PID
 {
-	/**
-	 * Create a new PID tracker item for the supplied job ID.
-	 *
-	 * @param string $id The ID of the job to track the PID of.
-	 */
-	public static function create($id)
-	{
-		Resque::redis()->set('job:' . $id . ':pid', (string)getmypid());
-	}
+    /**
+     * Create a new PID tracker item for the supplied job ID.
+     *
+     * @param string $id The ID of the job to track the PID of.
+     */
+    public static function create($id)
+    {
+        Resque::redis()->set('job:' . $id . ':pid', (string)getmypid());
+    }
 
-	/**
-	 * Fetch the PID for the process actually executing the job.
-	 *
-	 * @param string $id The ID of the job to get the PID of.
-	 *
-	 * @return int PID of the process doing the job (on non-forking OS, PID of the worker, otherwise forked PID).
-	 */
-	public static function get($id)
-	{
-		return (int)Resque::redis()->get('job:' . $id . ':pid');
-	}
+    /**
+     * Fetch the PID for the process actually executing the job.
+     *
+     * @param string $id The ID of the job to get the PID of.
+     *
+     * @return int PID of the process doing the job (on non-forking OS, PID of the worker, otherwise forked PID).
+     */
+    public static function get($id)
+    {
+        return (int)Resque::redis()->get('job:' . $id . ':pid');
+    }
 
-	/**
-	 * Remove the PID tracker for the job.
-	 *
-	 * @param string $id The ID of the job to remove the tracker from.
-	 */
-	public static function del($id)
-	{
-		Resque::redis()->del('job:' . $id . ':pid');
-	}
+    /**
+     * Remove the PID tracker for the job.
+     *
+     * @param string $id The ID of the job to remove the tracker from.
+     */
+    public static function del($id)
+    {
+        Resque::redis()->del('job:' . $id . ':pid');
+    }
 }
-
